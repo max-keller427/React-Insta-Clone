fd59f70c41cd0c8c93001c489092a3cc983ea5cf
max-keller427 <max.keller427@gmail.com>
2019-03-13 23:34:06 -0600

    added proptypes to username, imageUrl, and comments

--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import CommentSection from "../CommentSection/CommentSection";
 
 function PostContainer(props) {
-  // console.log(props);
+  console.log(props);
   return (
     <div>
       <div>
@@ -15,8 +16,27 @@ function PostContainer(props) {
           <CommentSection comment={comment} />
         ))}
       </div>
+      <div>
+        <p>Add a comment...</p>
+        <input type="text" />
+      </div>
     </div>
   );
 }
+
+PostContainer.propTypes = {
+  dummyData: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      comments: PropTypes.arrayOf(
+        PropTypes.shape({
+          username: PropTypes.string.isRequired,
+          text: PropTypes.string.isRequired
+        })
+      )
+    })
+  )
+};
 
 export default PostContainer;
